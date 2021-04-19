8315fe022d65627289e0d965ea27d76af0d10757
Thomas Leese <thomas@leese.io>
2021-04-19 11:20:03 +0100

    Add a task definition

--- main.tf
+++ main.tf
@@ -20,3 +20,48 @@ resource "aws_ecr_repository" "main" {
 resource "aws_ecs_cluster" "main" {
   name = "learning-ecs"
 }
+
+data "aws_iam_policy_document" "assume_role_policy" {
+  statement {
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["ecs-tasks.amazonaws.com"]
+    }
+  }
+}
+
+resource "aws_iam_role" "ecs_task_execution_role" {
+  name               = "ecs-task-execution-role"
+  assume_role_policy = data.aws_iam_policy_document.assume_role_policy.json
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_task_execution_role_policy" {
+  role       = aws_iam_role.ecs_task_execution_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
+
+resource "aws_ecs_task_definition" "main" {
+  family                   = aws_ecs_cluster.main.name
+  container_definitions    = jsonencode([
+    {
+      name = "${aws_ecs_cluster.main.name}-task"
+      image = aws_ecr_repository.main.repository_url
+      cpu = 256
+      memory = 512
+      essential = true
+      portMappings = [
+        {
+          containerPort = 4567
+          hostPort = 4567
+        }
+      ]
+    }
+  ])
+  requires_compatibilities = ["FARGATE"]
+  network_mode             = "awsvpc"
+  memory                   = 512
+  cpu                      = 256
+  execution_role_arn       = aws_iam_role.ecs_task_execution_role.arn
+}
